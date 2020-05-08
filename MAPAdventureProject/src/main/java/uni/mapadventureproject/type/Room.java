29972651baeb8de20ad09689bfec5a6083f78cd5
gianni-tauro <gianni.t95@live.it>
2020-05-08 13:31:03 +0200

    Creata la classe Room

---
+++ MAPAdventureProject/src/main/java/uni/mapadventureproject/type/Room.java
@@ -0,0 +1,153 @@
+package uni.mapadventureproject.type;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Admin
+ */
+public class Room {
+
+    private final int id;
+    private String name;
+    private String desc;
+    private String look;
+    private boolean visible = true;
+    private Room north = null;
+    private Room south = null;
+    private Room west = null;
+    private Room east = null;
+    private Room up = null;
+    private Room down = null;
+    private List<Item> itemList = new ArrayList<>();
+
+    public Room(int id, String name, String desc) {
+        this.id = id;
+        this.name = name;
+        this.desc = desc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getLook() {
+        return look;
+    }
+
+    public void setLook(String look) {
+        this.look = look;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+
+    public Room getNorth() {
+        return north;
+    }
+
+    public void setNorth(Room north) {
+        this.north = north;
+    }
+
+    public Room getSouth() {
+        return south;
+    }
+
+    public void setSouth(Room south) {
+        this.south = south;
+    }
+
+    public Room getWest() {
+        return west;
+    }
+
+    public void setWest(Room west) {
+        this.west = west;
+    }
+
+    public Room getEast() {
+        return east;
+    }
+
+    public void setEast(Room east) {
+        this.east = east;
+    }
+
+    public Room getUp() {
+        return up;
+    }
+
+    public void setUp(Room up) {
+        this.up = up;
+    }
+
+    public Room getDown() {
+        return down;
+    }
+
+    public void setDown(Room down) {
+        this.down = down;
+    }
+
+    public List<Item> getItemList() {
+        return itemList;
+    }
+
+    public void setItemList(List<Item> itemList) {
+        this.itemList = itemList;
+    }
+
+    public void addItem(Item i) {
+        itemList.add(i);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 19 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Room other = (Room) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+
+}
