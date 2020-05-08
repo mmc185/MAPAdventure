1711077dac4db57d0c550d82d9e0456ea848f843
Anna Marzella <annamarze99uni@gmail.com>
2020-05-08 13:44:54 +0200

    Corretto l'errore in ItemContainer

--- MAPAdventureProject/src/main/java/uni/mapadventureproject/type/ItemContainer.java
+++ MAPAdventureProject/src/main/java/uni/mapadventureproject/type/ItemContainer.java
@@ -2,12 +2,19 @@ package uni.mapadventureproject.type;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class ItemContainer extends Item {
 
     private List<Item> cItemList;
 
-    public ItemContainer(List<Item> cItemList) {
+    public ItemContainer(List<Item> cItemList, int id, String name, String desc) {
+        super(id, name, desc);
+        this.cItemList = new ArrayList<>();
+    }
+
+    public ItemContainer(List<Item> cItemList, int id, String name, String desc, Set<String> alias) {
+        super(id, name, desc, alias);
         this.cItemList = new ArrayList<>();
     }
 
