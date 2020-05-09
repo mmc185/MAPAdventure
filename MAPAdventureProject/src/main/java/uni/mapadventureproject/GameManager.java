3f2cec59a70abed82b52c77b4c75580435f64b14
gianni-tauro <gianni.t95@live.it>
2020-05-09 12:07:59 +0200

    Creata la classe GameManager e l'enumerativo WordType

---
+++ MAPAdventureProject/src/main/java/uni/mapadventureproject/GameManager.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uni.mapadventureproject;
+
+import java.util.Map;
+import uni.mapadventureproject.parser.WordType;
+import uni.mapadventureproject.type.Command;
+import uni.mapadventureproject.type.Inventory;
+import uni.mapadventureproject.type.Room;
+
+/**
+ *
+ * @author Admin
+ */
+public class GameManager {
+
+    public void executeCommand(Room currentRoom, Inventory inv, Command commands, Map<WordType, String> commandMap) {
+
+    }
+}
