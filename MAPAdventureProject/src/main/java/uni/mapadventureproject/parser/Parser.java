c3f21ffde37634854ac91894a8097dd9203b7175
mmc <martina.melerocavallo@outlook.com>
2020-06-15 12:49:43 +0200

    Implementato il Parser

--- MAPAdventureProject/src/main/java/uni/mapadventureproject/parser/Parser.java
+++ MAPAdventureProject/src/main/java/uni/mapadventureproject/parser/Parser.java
@@ -11,17 +11,73 @@ import uni.mapadventureproject.type.Room;
 public class Parser {
 
     public Map<WordType, String> parse(String phrase, Room currentRoom, Inventory inv, Set<Command> commands) {
+
+        // Formato da coppie di tipi di parola e stringa che lo identifica. (e.g. (OBJECT, "taralli") )
         Map<WordType, String> parsedData = new HashMap<>();
+
+        String[] tokens = phrase.toLowerCase().split("\\s+");
+
+        for (String t : tokens) {
+
+            if (this.isCommand(t, commands)) {
+
+                parsedData.put(WordType.COMMAND, t);
+
+            }
+
+        }
+
+        if (parsedData.size() == 1) {
+
+            for (String t : tokens) {
+
+                if (this.isItem(t, inv)) {
+
+                    parsedData.put(WordType.I_OBJ, t);
+
+                }
+
+                if (this.isItem(t, currentRoom.getItemList())) {
+
+                    parsedData.put(WordType.R_OBJ, t);
+
+                }
+
+            }
+
+        }
+
         return parsedData;
     }
 
-    public boolean isCommand() {
-        //TODO
+    private boolean isCommand(String s, Set<Command> commands) {
+
+        for (Command c : commands) {
+
+            if (c.getName().equals(s) || c.getAlias().contains(s)) {
+
+                return true;
+
+            }
+
+        }
+
         return false;
     }
 
-    public boolean isItem() {
-        //TODO
+    private boolean isItem(String s, Inventory inv) {
+
+        for (Item i : inv.getInventoryList()) {
+
+            if (i.getName().equals(s) || i.getAlias().contains(s)) {
+
+                return true;
+
+            }
+
+        }
+
         return false;
     }
+
 }
