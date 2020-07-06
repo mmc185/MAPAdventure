39cfa18d6625215d15f1ff84bf9067817ff2ccc8
gianni-tauro <gianni.t95@live.it>
2020-07-06 14:36:02 +0200

    Inserita la documentazione nelle classi del package type

--- MAPAdventureProject/src/main/java/uni/mapadventureproject/type/Inventory.java
+++ MAPAdventureProject/src/main/java/uni/mapadventureproject/type/Inventory.java
@@ -14,7 +14,7 @@ public class Inventory implements Serializable {
     public Inventory() {
         inventoryList = new ArrayList<>();
     }
-
+    //Metodi get e set per gli attributi
     public List<Item> getInventoryList() {
         return inventoryList;
     }
@@ -22,15 +22,28 @@ public class Inventory implements Serializable {
     public void setInventoryList(List<Item> inventoryList) {
         this.inventoryList = inventoryList;
     }
-
+    
+    /**
+     * Aggiunge un oggetto all'inventario
+     * @param i oggetto(item)da aggiungere 
+     */
     public void add(Item i) {
         inventoryList.add(i);
     }
-
+    
+    /**
+     * Rimuove un oggetto dall'inventario
+     * @param i oggetto da rimuovere
+     * @return  indica se un oggetto è rimuovibile dall'inventario o meno
+     */
     public boolean remove(Item i) {
         return inventoryList.remove(i);
     }
 
+    /**
+     * Elenca il contenuto dell'inventario del giocatore,della stanza o di un contenitore sotto forma di stringa
+     * @return Stringa che indica il contenuto di un oggetto di tipo Inventory
+     */
     @Override
     public String toString() {
 
@@ -43,6 +56,11 @@ public class Inventory implements Serializable {
         return invList.toString();
     }
 
+    /**
+     * Cerca un oggetto nella lista di oggetti contenuti dall'inventario
+     * @param iName nome oggetto
+     * @return i nel caso l'oggetto è presente nella lista e null altrimenti
+     */
     public Item searchItem(String iName) {
         for (Item i : inventoryList) {
             
